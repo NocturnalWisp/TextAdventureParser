432fc9c3863cd8a0ab2594322b7a25882fe8191a
TJ <rogersteve97@gmail.com>
2023-05-14 22:14:31 -0600

    Removed build

--- include/ta_objects/ta_event.hpp
+++ include/ta_objects/ta_event.hpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "../ta_object.hpp"
+#include "ta_action.hpp"
 
 class TAEvent : public TAObject
 {
@@ -14,27 +15,16 @@ private:
     }
 public:
     TAObject* name;
+    TAObject* desc = new TAString();
+    std::vector<TAAction*> actions;
 
-    TAObject& Parse(std::string& str, OptionalMap m) override
-    {
-        // for (auto s : items)
-            // std::cout << s << std::endl;
+    TAObject& Parse(std::string& str, OptionalMap m) override;
 
-        return *this;
-    }
+    TAObject* Create() override;
 
-    TAObject* Create() override
-    {
-        return new TAEvent;
-    };
-
-    std::string& getString() override
-    {
-        return name->getString();
-    }
-
-    std::string getClass() override
-    {
-        return "TAEvent";
-    }
+    std::string& getString() override;
+    std::string getClass() override;
+private:
+    void getDescription(OptionalMap m);
+    void getActions(OptionalMap m);
 };
